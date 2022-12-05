d17ffabf1a85c04fbfa8d95c6ffe152e17e43c2e
dexat0r <nikit-balak@yandex.ru>
2022-12-05 20:45:57 +0400

    Fixed callback saving and safe unsubscribing

--- demo/safePub-safeSub-demo.js
+++ demo/safePub-safeSub-demo.js
@@ -11,16 +11,18 @@ async function main() {
     const topic = "test-topic";
 
     const callback = (meta) => meta % 2 == 0 ? meta / 2 : (meta + 1) / 2
+    const callback2 = (meta) => meta % 2 == 0 ? meta * 2 : (meta + 1) * 2
 
     bus.safeSub(resource, topic, callback)
-
-    console.log(await bus.safePub(resource, topic, 2));
-    console.log(await bus.safePub(resource, topic, 5));
-
-    bus.safeUnsubscribe(resource, topic, callback) //??
+    bus.safeSub(resource, topic, callback2)
     
     console.log(await bus.safePub(resource, topic, 2));
     console.log(await bus.safePub(resource, topic, 5));
+    
+    bus.safeUnsubscribe(resource, topic, callback)
+    
+    console.log(await bus.safePub(resource, topic, 3));
+    console.log(await bus.safePub(resource, topic, 2));
 }
 
 main().catch(console.log);
